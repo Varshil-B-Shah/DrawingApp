e28a7d565c70a21a9426f68d4e17db6da7dfeb0f
Varshil <varshil4247shah@gmail.com>
2026-02-05 22:43:38 +0530

    started brushSize change functionality

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -7,6 +7,7 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import androidx.core.graphics.createBitmap
@@ -91,6 +92,14 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
         canvasPaint = Paint(Paint.DITHER_FLAG)
         brushSize = 20.toFloat()
+    }
+
+    fun changeBrushSize(newSize: Float) {
+        brushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics
+        )
+        drawPaint.strokeWidth = brushSize
     }
 
     internal class FingerPath(var color: Int, var brushThickness: Float) : Path() {
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,18 +3,15 @@ package com.example.drawingapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawingView: DrawingView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        drawingView = findViewById(R.id.drawing_view)
+        drawingView.changeBrushSize(50.toFloat())
     }
 }
