65da5bc9551e98f56085dcc2caabbbb5e1dc505e
Varshil <varshil4247shah@gmail.com>
2026-02-05 23:38:46 +0530

    brushSeekBar added

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,17 +1,55 @@
 package com.example.drawingapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawingView: DrawingView
+    private lateinit var brushButton: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        brushButton = findViewById(R.id.brush_button)
+
         drawingView = findViewById(R.id.drawing_view)
-        drawingView.changeBrushSize(50.toFloat())
+        drawingView.changeBrushSize(20.toFloat())
+
+        brushButton.setOnClickListener {
+            showBrushChooserDialog()
+        }
+    }
+
+    private fun showBrushChooserDialog() {
+        val brushDialog = Dialog(this@MainActivity)
+        brushDialog.setContentView(R.layout.dialog_brush)
+        val seekBarProgress = brushDialog.findViewById<SeekBar>(R.id.dialog_seek_bar)
+        val showProgressTv = brushDialog.findViewById<TextView>(R.id.dialog_text_view_progress)
+
+        seekBarProgress.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                seekBar: SeekBar,
+                p1: Int,
+                p2: Boolean
+            ) {
+                drawingView.changeBrushSize(seekBar.progress.toFloat())
+                showProgressTv.text = seekBar.progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+        brushDialog.show()
     }
 }
